32423068e5f5e720ddd168013d238c9db52ed225
Joaquin Morales <joaquin.moralestr@gmail.com>
2024-05-09 21:55:32 -0400

    Fraction program done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
+
+        Console.WriteLine(f1.GetTop());
+        f1.SetTop(3);
+        Console.WriteLine(f1.GetTop());
+
+        Console.WriteLine(f1.GetBottom());
+        f1.SetBottom(2);
+        Console.WriteLine(f1.GetBottom());
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }

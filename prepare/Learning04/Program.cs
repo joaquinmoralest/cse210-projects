00653344f454e05bd6624467d12ebc8c456bbbd1
Joaquin Morales <joaquin.moralestr@gmail.com>
2024-05-15 23:06:35 -0400

    Inheritance activity done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Joaquin Morales", "Programming");
+        MathAssignment mathAssignment = new MathAssignment("Juan Perez", "Fractions", "7.3", "8-19");
+        WrittingAssignment writtingAssignment = new WrittingAssignment("Elba Lazo", "English", "A great book");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine(writtingAssignment.GetSummary());
+        Console.WriteLine(writtingAssignment.GetWrittingInformation());
     }
 }
